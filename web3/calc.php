35523d90db14f34655de7442df9680ea7225a1e7
annacuadra <202080161@psu.palawan.edu.ph>
2022-09-21 16:17:26 +0800

    done Practice 5-02

--- web3/calc.php
+++ web3/calc.php
@@ -3,17 +3,31 @@
 <body>
     <?php
         # test if input is_numeric
-        if(1){
+        if(is_numeric($_POST['val1']) && (is_numeric($_POST['val2']))){
             # test if calc is not null
-            if(1){
-                # switch statement for value of calc
-                switch(1){
-                    
-                }
+            if(!empty($_POST['calc'])){
+                    # switch statement for value of calc
+                    switch($_POST['calc']){
+                        case 'add':
+                            $result = ($_POST['val1']) + ($_POST['val2']);
+                            break;
+                        case 'sub':
+                            $result = ($_POST['val1']) - ($_POST['val2']);
+                            break;
+                        case 'mul':
+                            $result = ($_POST['val1']) * ($_POST['val2']);
+                            break;
+                        case 'div':
+                            $result = ($_POST['val1']) / ($_POST['val2']);
+                            break;
+                    }
                 echo "Calculation result: $result";
+            } else {
+                echo "You didn't select any arithmentical operation. Please try again.";
             }
+        } else { 
+            echo "Invalid entry - please retry"; 
         }
-        else{ echo "Invalid entry - please retry"; }
     ?>
 </body>
 </html>
