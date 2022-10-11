7730b26fd7c147c2db5710280a2377431ad488f5
Eize's <202080164@psu.palawan.edu.ph>
2022-10-11 17:27:15 +0800

    done practice 5-04

--- web5/index.php
+++ web5/index.php
@@ -1,11 +1,25 @@
 <?php
-    if(isset($_COOKIE['views'])){
-        $count = $_COOKIE['views'];
-        $num_visits ++;
-    }
-    setcookie('views', $num_visits);
+session_start();
+
+$num_visits = $_SESSION['count'];
+if(isset($_GET['reset']) || !isset($num_visits)){
+    $num_visits = 0;
+}
+$num_visits++;
+$_SESSION['count'] = $num_visits;
 ?>
 
 <h1>Hello</h1>
-<p>You have visited this page <?= $num_visits ?> times</p>
+<p>You have visited this page <?= $num_visits ?> times</p> 
 <p><a href="?reset=true">Reset</a></p>
+
+
+<?php if($num_visits >= 5): ?>
+    <h2>Wow!</h2>
+    <p>You sure come to this page a lot.</p>
+	<?php endif; ?>
+
+<?php if($num_visits == 10): ?>
+	<?php
+header ("Location: congratulations.php"); ?>
+<?php endif; ?>
